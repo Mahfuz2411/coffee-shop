cb50831f533754fcd6a46b6f3f6d641e5f0e5aee
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-10-18 14:13:28 +0600

    Post method created

--- client/src/components/AddCoffee.jsx
+++ client/src/components/AddCoffee.jsx
@@ -1,7 +1,148 @@
 const AddCoffee = () => {
+  const handleAddCoffee = event =>{
+    event.preventDefault();
+
+    const form = event.target;
+
+    const name = form.Name.value;
+    const quantity = form.Quantity.value;
+    const suplier = form.Suplier.value;
+    const taste = form.Taste.value;
+    const category = form.Category.value;
+    const details = form.Details.value;
+    const photo = form.Photo.value;
+
+    const newCoffee = {name, quantity, suplier, taste, category, details, photo};
+
+    // send data to the server
+    fetch('http://localhost:5000/coffee', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(newCoffee)
+    })
+    .then(res => res.json())
+    .then(data => {console.log(data)})
+  }
   return (
-    <div>
-      <h1>Add a coffe</h1>
+    <div className="bg-[#F4F3F0] p-24">
+      <h1 className="text-3xl font-extrabold text-center">ADD A COFFEE</h1>
+      <form onSubmit={handleAddCoffee} action="">
+        {/* Form name and quantity row*/}
+        <div className="md:flex">
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Coffee Name</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Name"
+                className="input input-bordered join-item w-full"
+                placeholder="Coffee Name"
+              />
+            </div>
+          </div>
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Available Quantity</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Quantity"
+                className="input input-bordered join-item w-full"
+                placeholder="Available Quantity"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form suplier and taste row*/}
+        <div className="md:flex">
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Suplier</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Suplier"
+                className="input input-bordered join-item w-full"
+                placeholder="Suplier Name"
+              />
+            </div>
+          </div>
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Taste</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Taste"
+                className="input input-bordered join-item w-full"
+                placeholder="Taste"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form category and details row*/}
+        <div className="md:flex">
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Category</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Category"
+                className="input input-bordered join-item w-full"
+                placeholder="Category"
+              />
+            </div>
+          </div>
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Details</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Details"
+                className="input input-bordered join-item w-full"
+                placeholder="Details"
+              />
+            </div>
+          </div>
+        </div>
+        {/* Form photo url row*/}
+        <div className="md:flex">
+          <div className="form-control w-full p-10">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <div className="join">
+              <input
+                type="text"
+                name="Photo"
+                className="input input-bordered join-item w-full"
+                placeholder="Photo URL"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="form-control w-full p-10">
+          <div className="join">
+            <input
+              type="submit"
+              name="Submit"
+              className="btn btn-block btn-primary"
+              value="Add Coffee"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}>:${process.env.DB_PASS}@cluster0.9p7ok35.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9p7ok35.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -26,23 +26,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    
+    const coffeeCollection = client.db('coffeeDB').collection('coffee');
+
+
+        
+    app.get('/coffee', (req, res)=> {
+      res.send('Server Running.');
+    });
+
+
+
+    app.listen(port, ()=> {
+      console.log(`http://localhost:${port}/`);
+    });
+
+    app.post('/coffee', async(req, res)=>{
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
 
 
-
-app.get('/', (req, res)=> {
-  res.send('Server Running.');
-});
-
-app.listen(port, ()=> {
-  console.log(`http://localhost:${port}/`);
-});
