a0291235a79bc012a42b8f1990993981566b327d
Mahfuz2411 <mahfuzibnesyful24@gmail.com>
2023-10-18 11:59:31 +0600

    database added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-6xl text-purple-600">Coffee Shop</h1>
     </>
   )
 }
---
+++ client/src/components/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+  return (
+    <div>
+      <h1>Update a coffe</h1>
+    </div>
+  );
+};
+
+export default UpdateCoffee;
